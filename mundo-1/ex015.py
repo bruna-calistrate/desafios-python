5acb5fd455b7ca61a2428faa44f4d2e97c27f546
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:57:29 -0300

    Cálculo de aluguel de carro
    Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.

--- mundo-1/ex015.py
+++ mundo-1/ex015.py
@@ -1,7 +1,9 @@
 print('Curso Python ##Extra - Tratando dados e fazendo contas - Desafio 15\n\n'
       'Calculo de aluguel de carro')
+
 d = int(input('Insira a quantidade de dias que o carro foi alugado: '))
 km = float(input('Insira a quantidade de km rodados: '))
-print('O carro foi alugado por \033[1;34m{} dias\033[m e rodou \033[1;32m{:.2f} km\033[m.\n'
-      'O valor do aluguel será: \033[4;42m>> R$ {:.2f} <<\033[m'
+
+print('O carro foi alugado por {} dias e rodou {:.2f} km.\n'
+      'O valor do aluguel será: >> R$ {:.2f} <<'
       .format(d, km, d * 60 + km * 0.15))
