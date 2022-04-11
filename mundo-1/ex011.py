e65d7227b85498289f963e89f5824eee36e87cbc
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:50:30 -0300

    Cálculo de litros por área
    Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la sabendo que cada litro de tinta pinta uma área de 2m².

--- mundo-1/ex011.py
+++ mundo-1/ex011.py
@@ -6,6 +6,6 @@ alt = float(input('Insira a altura da parede em metros: '))
 area = lar * alt
 lit = area / 2
 
-print('Sua parede tem a dimensão de {:.3f} m x {:.3f} m e sua área é de {:.3f} m²\n'
+print('Sua parede tem a dimensão de {:.3f} m x {:.3f} m e sua área é de {:.3f} m² \n'
       'Serão necessários {:.3f} l de tinta para pintar essa parede.'
       .format(lar, alt, area, lit))
