90d1a72a4f2b6fc807083b3dd099d53b009772fc
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 12:04:28 -0300

    Sorteio 1.0
    Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.

--- mundo-1/ex019.py
+++ mundo-1/ex019.py
@@ -1,11 +1,13 @@
+from random import choice
+
 print('Curso Python #8 - Utilizando módulos - Desafio 19\n'
       'Vamos fazer um sorteio?\n')
-from random import choice
 
 a1 = str(input('Qual o nome do primeiro aluno? ')).strip().title()
 a2 = str(input('Qual o nome do segundo aluno? ')).strip().title()
 a3 = str(input('Qual o nome do terceiro aluno? ')).strip().title()
 a4 = str(input('Qual o nome do quarto aluno? ')).strip().title()
+
 lista = [a1, a2, a3, a4] # Listas devem estar entre colchetes
 print('\nEntre os alunos {}, {}, {} e {}, o sorteado para apagar o quadro é:\n'
-      '\033[1;30;42m>>> {} <<<\033[m'.format(a1, a2, a3, a4, choice(lista)))
+      '>>> {} <<<'.format(a1, a2, a3, a4, choice(lista)))
