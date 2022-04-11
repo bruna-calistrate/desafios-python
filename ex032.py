fcdb52069e91f26ef53cc9f5c5528ee705d60993
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:05:53 -0300

    É ano bissexto?
    Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.

---
+++ ex032.py
@@ -0,0 +1,23 @@
+print("""Curso Python #10 - Condições Simples e Compostas - Desafio 32
+
+É um ano bissexto?
+""")
+from datetime import date
+
+ano = int(input("Digite o ano que quer analizar ou 0 para o ano atual: "))
+
+if ano == 0:
+    ano = date.today().year
+    
+bis = ano % 4 == 0 and ano % 100 !=0 or ano % 400 == 0
+temp = "será"
+
+if ano == date.today().year:
+    temp = "é"
+if ano < date.today().year:
+    temp = "foi"
+
+if bis == True:
+    print(f"O ano {ano} {temp} bissexto!")
+else:
+    print(f"O ano {ano} não {temp} bissexto.")
