58b47e76cada5c61d47d388cb81a082d4d94b1bd
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:42:57 -0300

    Cálculo de média
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

--- mundo-1/ex007.py
+++ mundo-1/ex007.py
@@ -6,4 +6,4 @@ n1 = float(input('Insira a primeira nota: '))
 n2 = float(input('Insira a segunda nota: '))
 m = (n1 + n2) / 2
 
-print('A média das notas é \033[1;35m>>{:.2f}<<\033[m.'.format(m))
+print('A média das notas é >>{:.2f}<<.'.format(m))
