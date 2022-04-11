fd8a446c980c8dab765fd4d5166dfee5506d5db5
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:52:55 -0300

    Cálculo de aumento
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.

---
+++ ex013.py
@@ -0,0 +1,6 @@
+print('Curso Python #7 - Operadores aritméticos - Desafio 13\n'
+      'Aumento de salário em 15%')
+sal = float(input('Insira o seu salário atual: R$ '))
+aum = sal * 1.15
+print('O seu novo salário é \033[4;32mR$ {:.2f}\033[m!'.format(aum))
+
