e6c276647904ec9b189e11c7c8604772f8f8592e
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 12:13:25 -0300

    Reprodutor de música
    Faça um programa em Python que abra e reproduza o áudio de um arquivo mp3.

--- mundo-1/ex021.py
+++ mundo-1/ex021.py
@@ -1,11 +1,11 @@
+from pygame import mixer
+
 print('Curso Python #8 - Utilizando módulos - Desafio 21\n'
       'Reproduzindo uma música\n\n')
-
-from pygame import mixer
 
 mixer.init()
 mixer.music.load('ex021_mus.mp3')
 # mus = input('Insira o caminho do arquivo .mp3: ')
 # mixer.music.load(mus)
 mixer.music.play()
-input('\033[2;32mEstá ouvindo agora?\033[m')
+input('Está ouvindo agora?')
