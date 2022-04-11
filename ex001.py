0ae6b448a188da6086ec2f2ba91dfd06f4ba7521
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:13:35 -0300

    Qual o seu nome?

---
+++ ex001.py
@@ -0,0 +1,10 @@
+# Lembrar de fazer um head para ficar mais bunitin
+print('\033[1;40mCurso Python Mundo 1 - Aula #4 - Primeiros comandos - Desafio 01\33[m')
+# Quando entre aspas, inserir os espaços para que o comando fique mais organizado
+nome = str(input("Qual o seu nome? ")).strip().title()
+print('Bem vinde, {}!'.format(nome))
+# formato anterior, mais manual print('Bem vinde,', nome + '!')
+import emoji
+print(emoji.emojize("Obrigada por testar comigo! :smiley:", use_aliases=True))
+
+
