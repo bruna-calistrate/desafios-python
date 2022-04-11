c4bc034db9cab65d84e1ea2fdcd901e060027562
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 14:14:00 -0300

    Cálculo condicional: par ou ímpar?
    Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.

--- mundo-1/ex030.py
+++ mundo-1/ex030.py
@@ -5,7 +5,8 @@ Vou adivinhar se seu número é PAR ou ÍMPAR!
 
 num = int(input("Digite um número inteiro: "))
 par = num % 2
+
 if par == 0:
-    print(f"O número {num} é \033[1;34mpar\033[m!")
+    print(f"O número {num} é par!")
 else:
-    print(f"O número {num} é \033[1;33mímpar\033[m.")
+    print(f"O número {num} é ímpar\.")
