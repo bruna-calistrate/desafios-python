b42247ac35dc2f342a33c8ab3537f5bb5ace75f4
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:19:30 -0300

    Sucessor e Antecessor

---
+++ ex005.py
@@ -0,0 +1,6 @@
+print('Curso Python #7 - Operadores aritméticos - Desafio 05\n')
+n1 = int(input('Insira um número: '))
+sus = n1 + 1
+ant = n1 - 1
+print('\nO número é \033[1;32m{}\033[m, seu sucessor é \033[1;34m{}\033[m e seu antecessor é \033[1;33m{}\033[m.'.format(n1, sus, ant))
+
