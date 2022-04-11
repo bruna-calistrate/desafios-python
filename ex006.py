575810346687896b7880ae5bbefeb13840a366f3
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:38:50 -0300

    Dobro, triplo e raiz
    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

--- ex006.py
+++ ex006.py
@@ -1,8 +1,10 @@
 print('Curso Python #7 - Operadores aritméticos - Desafio 06\n')
+
 n1 = float(input('Insira um número: '))
 dobro = n1 * 2
 triplo = n1 * 3
 raiz = n1 ** (1/2)
+
 print("""O seu número é \033[1;32m{}\033[m, 
 o seu dobro é \033[1;31m{}\033[m, 
 o seu triplo é \033[1;33m{}\033[m e 
