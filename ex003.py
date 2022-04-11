17575fa14e13a0b2c01ab68254767895cce6da0d
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:33:33 -0300

    Vamos somar!
    Script que leia dois números e tenta mostrar a soma entre eles

--- ex003.py
+++ ex003.py
@@ -1,6 +1,7 @@
 print('Curso Python #4 - Primeiros comandos - Desafio 03')
 print('')
 print('Vamos fazer uma operação de soma?')
+
 num01 = input('Insira o primeiro número: ')
 num02 = input('Insira o segundo número: ')
 cores = {'limpa': '\033[m',
@@ -10,12 +11,11 @@ print('')
 print(f"O resultado é: {cores['verm']}{num01+num02}{cores['limpa']}")
 
 # isso apenas junta os dois números, concatenação, é necessária uma nova função para executar o código corretamente
-
-
 print(f"Ops, algo errado não está certo... vamos tentar novamente?")
 print('Processando...')
 from time import sleep
 sleep(5)
+
 # função int determina que o valor será um número inteiro
 print('')
 total = int(num01) + int(num02)
