03934dd72f09486574ec4eae1a58b182becd492a
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:56:15 -0300

    Conversor de temperaturas
    Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit

--- mundo-1/ex014.py
+++ mundo-1/ex014.py
@@ -7,19 +7,19 @@ if temp == 'C':
       c = float(input('Insira a temperatura em Celsius para conversão: '))
       f = (c * 1.8) + 32
       k = c + 273
-      print('A temperatura \033[1;34m{:.1f}°C\033[m corresponde a:\n'
-      '\033[1;33m{:.1f}° Fahrenheit\033[m e \033[1;35m{:.1f} Kelvin\033[m.\n'
+      print('A temperatura {:.1f}°C corresponde a:\n'
+      '{:.1f}° Fahrenheit e {:.1f} Kelvin.\n'
       '{:-^30}'.format(c, f, k, ''))
 if temp == 'F':
       f = float(input('Insira a temperatura em Fahrenheit para conversão: '))
       c = (f - 32) / 1.8
       k = c + 273
-      print('A temperatura \033[1;33m{:.1f}°F\033[m corresponde a:\n'
-      '\033[1;34m{:.1f}° Celsius\033[m e \033[1;35m{:.1f} Kelvin\033[m.\n'
+      print('A temperatura {:.1f}°F corresponde a:\n'
+      '{:.1f}° Celsius e {:.1f} Kelvin.\n'
       '{:-^30}'.format(f, c, k, ''))
 if temp == 'K':
       k = float(input('Insira a temperatura em Kelvin para conversão: '))
       c = k - 273
       f = (c * 1.8) + 32
-      print('A temperatura \033[1;35m{:.1f} Kelvin\033[m corresponde a:\n'
-      '\033[1;34m{:.1f}° Celsius\033[m e \033[1;33m{:.1f}° Fahrenheit\033[m.'.format(k, c, f))
+      print('A temperatura {:.1f} Kelvin corresponde a:\n'
+      '{:.1f}° Celsius e {:.1f}° Fahrenheit.'.format(k, c, f))
