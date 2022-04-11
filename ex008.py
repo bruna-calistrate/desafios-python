2736ebb119fc3f9bc4657d209fd77f48df3528a5
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:41:04 -0300

    Conversão de medidas
    Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros. No final faça com todas as medidas.

--- ex008.py
+++ ex008.py
@@ -1,5 +1,6 @@
 print('Curso Python #7 - Operadores aritméticos - Desafio 08\n')
 print('Vamos fazer uma conversão de metros para as outras formas de medida\n')
+
 m = float(input('Medida em metros: '))
 km = m * 0.001
 hm = m * 0.01
@@ -8,11 +9,13 @@ dm = m * 10
 cm = m * 100
 mm = m * 1000
 
-print("""A sua medida em metros é \033[4;31m{:.3f}\033[m m, 
+print("""A sua medida em metros é \033[4;31m{:.3f}\033[m m.
+
       Descendo as casas, a sua medida é:
       Em decímetros \033[4;33m{:.3f}\033[m dm,
       Em centímetros \033[4;32m{:.3f}\033[m cm e
       Em milímetros \033[4;34m{:.3f}\033[m mm.
+      
       Subindo as casas, a sua medida é:
       Em decâmetros \033[4;36m{:.3f}\033[m dam,
       Em hectômetros \033[4;35m{:.3f}\033[m hm e
