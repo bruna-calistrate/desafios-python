ad5d9ae64956864e903660b680412aa1915a1b10
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 14:12:34 -0300

    Cálculo condicional: Você vai ser multado?
    Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por cada km acima do limite.

--- mundo-1/ex029.py
+++ mundo-1/ex029.py
@@ -7,6 +7,6 @@ km = float(input('Qual a velocidade atual do seu veículo? '))
 multa = (km - 80) * 7
 
 if km > 80:
-    print(f"\033[1;30;41m Você está acima do limite de 80 km/h!! Você deverá pagar uma multa de R$ {multa:.2f}!! \033[m")
+    print(f"Você está acima do limite de 80 km/h!! Você deverá pagar uma multa de R$ {multa:.2f}!!")
 else:
-    print("\033[1;30;42m Tenha um bom dia! Dirija com segurança! \033[m")
+    print("Tenha um bom dia! Dirija com segurança!")
