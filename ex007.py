ca8936a8e09dfa112952dbd37e826889cf5a9450
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:39:41 -0300

    Média das notas
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

--- ex007.py
+++ ex007.py
@@ -1,6 +1,9 @@
 print('Curso Python #7 - Operadores aritméticos - Desafio 07\n')
+
 print('Vamos calcular a média de suas notas?')
+
 n1 = float(input('Insira a primeira nota: '))
 n2 = float(input('Insira a segunda nota: '))
 m = (n1 + n2) / 2
+
 print('A média das notas é \033[1;35m>>{:.2f}<<\033[m.'.format(m))
