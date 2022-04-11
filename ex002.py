e471f1b8913af369ef5b288d53fe1f7023e3dde2
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 09:14:34 -0300

    Quando você nasceu?

---
+++ ex002.py
@@ -0,0 +1,10 @@
+print('Curso Python #4 - Primeiros comandos - Desafio 02')
+print('')
+dia = int(input('Em qual dia você nasceu? '))
+mes = int(input('Em qual mês você nasceu? '))
+ano = int(input('Em qual ano você nasceu? '))
+print('')
+# Lembrar de sempre por + ou , para conectar todos os elementos do comando
+print(f'Você nasceu em \033[7;36;40m{dia:0>2}/{mes:0>2}/{ano}\033[m')
+print('')
+print('Obrigada!')
