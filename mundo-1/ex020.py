909559895cd19d2119dad02efb5b92a084c0bdb6
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 12:11:59 -0300

    Sorteio 2.0
    O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.

--- mundo-1/ex020.py
+++ mundo-1/ex020.py
@@ -1,6 +1,7 @@
+from random import sample, shuffle
+
 print('Curso Python #8 - Utilizando módulos - Desafio 20\n'
       'Vamos sortear a ordem de apresentação?\n')
-from random import sample, shuffle
 
 a1 = str(input('Qual o nome do primeiro aluno? ')).strip().title()
 a2 = str(input('Qual o nome do segundo aluno? ')).strip().title()
@@ -9,8 +10,9 @@ a4 = str(input('Qual o nome do quarto aluno? ')).strip().title()
 deck = [a1, a2, a3, a4]
 
 print('A ordem de apresentação dos alunos {}, {}, {} e {} será:\n'
-      '\033[1;30;44m{}\033[m.\n'.format(a1, a2, a3, a4, sample(deck, k=4)))
+      '{}.\n'.format(a1, a2, a3, a4, sample(deck, k=4)))
 
-shuffle(deck) # Embaralha a lista, não precisa jogar como função
+# Embaralha a lista, não precisa jogar como função
+shuffle(deck) 
 print('Outra maneira é: \n'
-      '\033[1;30;45m{}\033[m.'.format(deck))
+      '{}.'.format(deck))
