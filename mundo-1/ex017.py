e8e8ba35e935ed371709131d1217382eea8b268a
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 12:00:29 -0300

    Cálculo da hipotenusa
    Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.

--- mundo-1/ex017.py
+++ mundo-1/ex017.py
@@ -1,9 +1,9 @@
+from math import hypot
+
 print('Curso Python #8 - Utilizando módulos - Desafio 17\n'
       'Vamos calcular o valor da hipotenusa de um triângulo retângulo?\n')
-from math import hypot
+
 cat_a = float(input('Insira o valor do cateto oposto: '))
 cat_b = float(input('Insira o valor do cateto adjacente: '))
-print('A hipotenusa com os catetos \033[1;32m{}\033[m e \033[1;32m{}\033[m é igual a \033[1;35m{:.3f}\033[m.'
+print('A hipotenusa com os catetos {} e {} é igual a {:.3f}.'
       .format(cat_a, cat_b, hypot(cat_a, cat_b)))
-
-# A soma dos quadrados dos catetos é o quadrado da hipotenusa
