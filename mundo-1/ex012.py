e4408a2866329f060717d594f1db21132fc1fcc0
Bruna Calistrate <94569229+bruna-calistrate@users.noreply.github.com>
2022-04-11 11:52:50 -0300

    Cálculo de desconto 2.0
    Faça um algoritmo que leia o preço de um produto e mostre seu novo valor, com 5% de desconto.

--- mundo-1/ex012.py
+++ mundo-1/ex012.py
@@ -1,14 +1,17 @@
 print('Curso Python #7 - Operadores aritméticos - Desafio 12\n\n'
       'PROMOÇÃO!! DESCONTO DE 5%\n')
+
 val = float(input('Qual o valor atual do produto? R$ '))
 des = val * .95
-print('O produto que custava \033[1;31mR$ {:.2f}\033[m, com desconto de 5%, '
-      'agora vale \033[1;32mR$ {:.2f}\033[m!'.format(val, des))
+print('O produto que custava R$ {:.2f}, com desconto de 5%, '
+      'agora vale R$ {:.2f}!'.format(val, des))
+
 print('Agora vamos calcular o pagamento à vista e à prazo!')
 vista = val * 0.9
-print('Pagando à vista, o produto custará: \033[1;42mR$ {:.2f}\033[m!\n\n'.format(vista))
+print('Pagando à vista, o produto custará: R$ {:.2f}!\n\n'.format(vista))
+
 n = int(input('Quantas parcelas? '))
 par = (val / n) + (n * 0.5)
 
-print('Pagando em \033[1m{}\033[m parcelas, cada uma custará \033[1;35mR$ {:.2f}\033[m, totalizando \033[1;31mR$ {:.2f}\033[m.'
+print('Pagando em {} parcelas, cada uma custará R$ {:.2f}, totalizando R$ {:.2f}.'
       .format(n, par, n * par))
